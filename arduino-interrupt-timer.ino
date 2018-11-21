06ba27d08b810b18bbf0a339e52f622e5323de92
y-du <42994541+y-du@users.noreply.github.com>
2018-11-21 21:03:58 +0100

    fixed passiv buzzer, tweaked timings

--- arduino-interrupt-timer.ino
+++ arduino-interrupt-timer.ino
@@ -6,6 +6,7 @@ const int buzz_pin = 8;
 volatile unsigned int seconds;
 volatile unsigned int dip_1_state;
 int unsigned led_state;
+int unsigned buzz_state;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -20,6 +21,7 @@ void setup() {
   seconds = 0;
   dip_1_state = 0;
   led_state = 0;
+  buzz_state = 0;
   TCCR1A = 0;
   TCCR1B = 0;
   TIMSK1 |= (1<<TOIE1);
@@ -61,12 +63,12 @@ void loop() {
     long on_time = 0;
     long off_time = 0;
     while (seconds - start < duration) {
-      if ((led_state == 0) && (millis() - off_time >= 50)) {
+      if ((led_state == 0) && (millis() - off_time >= 70)) {
         digitalWrite(led_pin, HIGH);
         led_state = 1;
         on_time = millis();
       }
-      if ((led_state == 1) && (millis() - on_time >= 50)) {
+      if ((led_state == 1) && (millis() - on_time >= 70)) {
         digitalWrite(led_pin, LOW);
         led_state = 0;
         off_time = millis();
@@ -83,24 +85,37 @@ void loop() {
     digitalWrite(LED_BUILTIN, LOW);
     on_time = 0;
     off_time = 0;
+    long b_on_time = 0;
+    long b_off_time = 0;
     while (dip_1_state > 0) {
       if ((led_state == 0) && (millis() - off_time >= 400)) {
         digitalWrite(led_pin, HIGH);
-        digitalWrite(buzz_pin, LOW);
         led_state = 1;
         on_time = millis();
       }
+      if ((buzz_state == 0) && (millis() - b_off_time >= 1)) {
+        digitalWrite(buzz_pin, LOW);
+        buzz_state = 1;
+        b_on_time = millis();
+      }
       if ((led_state == 1) && (millis() - on_time >= 800)) {
         digitalWrite(led_pin, LOW);
-        digitalWrite(buzz_pin, HIGH);
         led_state = 0;
         off_time = millis();
+      }
+      if ((buzz_state == 1) && (millis() - b_on_time >= 1)) {
+        digitalWrite(buzz_pin, HIGH);
+        buzz_state = 0;
+        b_off_time = millis();
       }
     }
     if (led_state == 1) {
         digitalWrite(led_pin, LOW);
-        digitalWrite(buzz_pin, HIGH);
         led_state = 0;
+    }
+    if (buzz_state == 1) {
+        digitalWrite(buzz_pin, HIGH);
+        buzz_state = 0;
     }
   }
 }
